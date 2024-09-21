9d3a0eb82ed1a14005a5e52ce2753a640893a6af
samueltingg <samuelting7@gmail.com>
2024-09-21 21:15:21 +0800

    feat: changed box.command('start'... to box.start()

--- Tg-bot/tg_bot.js
+++ Tg-bot/tg_bot.js
@@ -2,15 +2,16 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf('7686574059:AAGWqHNcI-s_FrWdU2qZFy6-QYlCSR-Bv4s');
 console.log('hello');
-bot.command('start', async (ctx) => {
-  await ctx.reply('Welcome to my bot!');
-  await ctx.reply('Click the button to open the mini app:', {
+
+bot.start((ctx) => {
+  ctx.reply('Welcome to my bot!');
+  ctx.reply('Click the button to open the mini app:', {
     reply_markup: {
       inline_keyboard: [
         [{ text: 'Open Mini App', web_app: { url: 'https://www.google.com'} }],
       ],
     },
   });
-});
+})
 
 bot.launch();
