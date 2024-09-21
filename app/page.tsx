a4d3df24612744867531aaa34e8bd31f22ae29a1
Lewis lee <minecraftpe132006@gmail.com>
2024-09-22 06:05:42 +0800

    fix: something

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
+"use client";
 // app/page.tsx
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
 import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+
+import { useEffect, useState } from "react";
+import {
+  useTelegramLogin,
+  useDynamicContext,
+} from "../lib/dynamic";
+
+
 
 const MainContent = dynamic(() => import('./components/MainContent'), { ssr: false })
 
 export default function Home() {
+  const { sdkHasLoaded, user } = useDynamicContext();
+  // const { telegramSignIn } = useTelegramLogin();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (!sdkHasLoaded) return;
+
+    const signIn = async () => {
+      if (!user) {
+        await telegramSignIn({ forceCreateUser: true });
+      }
+      setIsLoading(false);
+    };
+
+    signIn();
+  }, [sdkHasLoaded]);
   return (
     <div className="bg-white min-h-screen flex flex-col">
       <header className="w-full p-4 bg-gray-100">
