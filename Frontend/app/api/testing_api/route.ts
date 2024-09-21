2f8f971f64ec33ff24dd07db54a30bd30454978a
zyeoh883 <yeohzishen2002@gmail.com>
2024-09-21 17:32:40 +0800

    feat: added POST to MongoDB for testing and handled useEffect for double trigger

--- Frontend/app/api/testing_api/route.ts
+++ Frontend/app/api/testing_api/route.ts
@@ -1,40 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 import clientPromise from '/lib/mongodb';
 
-// export async function POST(request: NextRequest) {
-//   try {
-//     const client = await clientPromise;
-//     const db = client.db('telegram_users');
-//     let data = await request.json();
-
-//     const emailExist = await db
-//       .collection('test')
-//       .findOne({ email: data.email });
-//     if (!emailExist) {
-//       return NextResponse.json(
-//         { message: 'Email not found. Please use a registered email.' },
-//         { status: 404 },
-//       );
-//     }
-//     return NextResponse.json(
-//       { message: 'Proceeding with email: ' + data.email, user: emailExist },
-//       { status: 200 },
-//     );
-//   } catch (e) {
-//     return NextResponse.json(
-//       { message: 'Internal server error' },
-//       { status: 500 },
-//     );
-//   }
-// }
-
-// export async function GET() {
-// 	return NextResponse.json({ message: "Hello from the API!"}, {status: 200});
-//   }
 
 export async function GET(req: NextRequest) {
 	try {
-		console.log("entered!!!")
+		console.log("entered GET!!!")
 		const client = await clientPromise;
 		const db = client.db('telegram_users');
 		const collection = await db.collection('test');
@@ -56,3 +26,29 @@ export async function GET(req: NextRequest) {
 }
 
 
+export async function POST(request: NextRequest)
+{
+	try {
+        console.log("entered POST!!!")
+        const client = await clientPromise;
+        const db = client.db("telegram_users");
+		const collection = await db.collection('test');
+        const value = await request.json();
+
+		const document = await collection.findOne({ test: value });
+
+        if (!value) {
+            throw new Error('Missing required fields');
+        }
+        if (document) {
+            throw new Error("Admin already exist in database");
+        }
+
+        await db.collection("test").insertOne({test: value});
+
+		return NextResponse.json({ message: "Saved to DB Successfully!!!"}, {status: 200});
+    } catch (error) {
+        console.error('Error saving to DB:', error);
+        return NextResponse.json({ message: "Failed to save value to database."}, {status: 400});
+    }
+}
--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,33 +1,61 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef} from 'react';
 
 export default function Home() {
   const [message, setMessage] = useState<string | null>(null);
+  const effectRan = useRef(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/testing_api', {
-			method:"GET"
-		});
-		console.log("heree the 1st");
-        const data = await response.json();
+	if (effectRan.current === false) { // to ensure UseEffect doesn't run twice during development
+		const fetchData = async () => {
+		try {
+			const response = await fetch('/api/testing_api', {
+				method:"GET"
+			});
+			console.log("heree the 1st");
+			const data = await response.json();
 
-		console.log("heree");
+			console.log("heree");
 
-        if (response.status == 200) {
-          setMessage(data.message);
-        } else {
-          setMessage("missing");
-        }
-		console.log("end")
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+			if (response.status == 200) {
+			setMessage(data.message);
+			} else {
+			setMessage("missing");
+			}
+			console.log("end")
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
+		};
 
-    fetchData();
+		const uploadData = async (value : string) => {
+			try {
+			const response = await fetch('/api/testing_api', {
+				method:"POST",
+				body: JSON.stringify(value)
+			});
+			console.log("heree the 1st");
+			const data = await response.json();
+	
+			console.log("heree");
+	
+			if (response.status == 200) {
+				setMessage(data.message);
+			} else {
+				setMessage("missing");
+			}
+			console.log("end POST")
+			} catch (error) {
+			console.error('Error posting data:', error);
+			}
+		};
+
+		uploadData("Workinggg!!!!");
+		return () => {
+			effectRan.current = true;
+		};
+	}
   }, []);
 
   return <div>{message}</div>;
